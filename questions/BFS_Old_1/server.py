b64c9167bba6092edee6a7bfff7ee7ace5ad82c6
Dev Ahluwalia <dahluwalia@berkeley.edu>
2023-11-24 10:17:01 -0500

    assessments and questions edit

--- questions/BFS_Old_1/server.py
+++ questions/BFS_Old_1/server.py
@@ -11,4 +11,4 @@ def grade(data):
     for j in range(min(len(correct_answer), len(student_answer))):
         if correct_answer[j] == student_answer[j]:
             score += 1
-    data["score"] = score/(len(correct_answer))
+    data["score"] = score/(max(len(student_answer), len(correct_answer)))
