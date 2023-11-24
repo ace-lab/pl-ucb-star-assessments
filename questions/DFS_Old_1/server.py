ee057665f447eb019e15d497e15a0bee3c8a150f
Dev Ahluwalia <dahluwalia@berkeley.edu>
2023-11-24 01:57:18 -0500

    assessments and questions done

---
+++ questions/DFS_Old_1/server.py
@@ -0,0 +1,14 @@
+import random
+
+
+def generate(data):
+    data["params"]["dot_notation"] = "Your dot notation here"
+
+def grade(data):
+    correct_answer = "ABDEFHIGC"
+    student_answer = data["submitted_answers"]["user_answer"]
+    score = 0
+    for j in range(min(len(correct_answer), len(student_answer))):
+        if correct_answer[j] == student_answer[j]:
+            score += 1
+    data["score"] = score/(len(correct_answer))
